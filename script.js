e6df4a93135bc33ef7cfae97b54614e291aaf390
LiraV <51744382+LiraV@users.noreply.github.com>
2023-11-24 18:20:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+let key
+let node_for_insert = document.getElementById("node_for_insert")
+for (key in content) {
+    node_for_insert.innerHTML += `
+    <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
+    <img style="width: 180px" class="align-self-center" src=${content[key].img}>
+    <h5 class="card-title">${content[key].title}</h5>
+    </li>
+}
